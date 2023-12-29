ad856cebaad6bf22d1bcd872da3fab6cb87487ef
Amit Kumar <amitraj8655977@gmail.com>
2023-12-29 18:27:42 +0530

    middlewareWithCloudinary

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,3 +1,4 @@
+
 class ApiError extends Error {
     constructor(
         statusCode,
@@ -12,8 +13,8 @@ class ApiError extends Error {
         this.success = false;
         this.errors = errors
 
-        if(statck) {
-            this.stack = statck
+        if(stack) {
+            this.stack = stack
         }else {
             Error.captureStackTrace(this,this,constructor)
         }
